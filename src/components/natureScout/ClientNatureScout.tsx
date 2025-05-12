c6b2ecf6aea5f1e4daea1643f0521a5100bdee7a
peter aichner <peter.aichner@crystal-design.com>
2025-05-12 16:19:05 +0200

    Füge neue Komponenten für die NaturScout-Funktionalität hinzu
    - Implementiere die `ClientNatureScout`-Komponente, die die `NatureScout`-Komponente dynamisch lädt, um die Server-Side-Rendering-Funktionalität zu optimieren.
    - Füge die `GetImage`, `HabitatAnalysis`, `LocationDetermination`, `UploadedImageList`, `UploadImages`, `Welcome`, `ParameterHeading` und `Summary`-Komponenten hinzu, um die Benutzeroberfläche für die Erfassung und Analyse von Habitatdaten zu erweitern.
    - Implementiere die `MapNoSSR`-Komponente, um eine interaktive Karte mit Polygon-Zeichnungsfunktionalität zu ermöglichen und die Benutzererfahrung durch dynamisches Laden zu verbessern.
    - Bereinige die Importpfade und strukturiere die Komponenten in `src/components/natureScout`, um die Lesbarkeit und Wartbarkeit des Codes zu erhöhen.

--- src/components/natureScout/ClientNatureScout.tsx
+++ src/components/natureScout/ClientNatureScout.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 
 const NatureScout = dynamic(
-  () => import('@/components/NatureScouts/NatureScout'),
+  () => import('@/components/natureScout/NatureScout'),
   { ssr: false }
 );
 
